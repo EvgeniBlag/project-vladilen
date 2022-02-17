c115a32d7871e0ba2e9c107e8db4a8ad8bace6eb
EvgeniBlag <blagodarni@gmail.com>
2022-02-17 11:30:07 +0100

    inishon

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import{BrowserRouter, Route,Switch} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+
+  <BrowserRouter>
+
+    <div className="container pt-4">
+       <h1>test</h1>
+    </div>
+
+  </BrowserRouter>
+  );
+}
+
+export default App;
